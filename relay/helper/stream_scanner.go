5c783b89cb7709c4f33c4008a774949834574849
nobody <nobody@noreply.com>
2026-06-10 12:51:19 +0800

    doc: add comment

--- relay/helper/stream_scanner.go
+++ relay/helper/stream_scanner.go
@@ -46,6 +46,7 @@ func StreamScannerHandler(c *gin.Context, resp *http.Response, info *relaycommon
 		return
 	}
 
+	// 无条件新建 StreamStatus
 	info.StreamStatus = relaycommon.NewStreamStatus()
 
 	// 确保响应体总是被关闭
